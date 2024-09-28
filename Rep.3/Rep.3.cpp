ee05e87c8ff56d18bc31f9acf75c775045e76273
Олександр <goncars451@gmail.com>
2024-09-28 14:17:54 +0300

    Добавьте файлы проекта.

---
+++ Rep.3/Rep.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, ".1251");
+    int num;
+
+    cout << "Введiть цiле число: ";
+    cin >> num;
+
+    cout << "Вихiдний номер: " << num << endl;
+
+    cout << "Постiнкремент: " << num++ << endl;
+
+    cout << "Пiсля постiнкременту: " << num << endl;
+
+    cout << "Попереднє збiльшення: " << ++num << endl;
+
+    cout << "Постдекремент: " << num-- << endl;
+
+    cout << "Пiсля постдекременту: " << num << endl;
+
+    cout << "Попереднє зменшення: " << --num << endl;
+
+    return 0;
+}
